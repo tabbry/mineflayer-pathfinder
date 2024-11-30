7388b1a4cd5fbb3373ffe0c8b68fce420ed739bd
tabbry <bryan.tabinas@gmail.com>
2024-11-30 15:14:55 +0100

    created simplified world

---
+++ lib/PathNode.ts
@@ -0,0 +1,19 @@
+﻿export class PathNode {
+    data: any | null = null
+    g: number = 0
+    h: number = 0
+    f: number = 0
+    parent: PathNode | null
+
+    constructor() {
+    }
+
+    set(data, g, h, parent = null) {
+        this.data = data;
+        this.g = g;
+        this.h = h;
+        this.f = g + h;
+        this.parent = parent;
+        return this;
+    }
+}
---
+++ lib/SimplifiedWorld.ts
@@ -0,0 +1,11 @@
+﻿import { Bot } from "../node_modules/mineflayer/index";
+import { Vec3 } from "../node_modules/vec3/index";
+
+
+export class SimplifiedWorld {
+    private readonly bot: Bot;
+
+    getBlock(pos: Vec3) {
+        return this.bot.blockAt(pos);
+    }
+}
